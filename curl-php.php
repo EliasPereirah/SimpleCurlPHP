e5869ae0c27d8188217c78d4057363aa862ff78d
Elias <16616409+EliasPereirah@users.noreply.github.com>
2022-10-10 12:35:26 -0300

    Update curl-php.php

--- curl-php.php
+++ curl-php.php
@@ -4,7 +4,7 @@ function getHTML($url)
    $ch = curl_init();
    curl_setopt($ch, CURLOPT_URL, $url);
    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-   curl_setopt($ch, CURLOPT_HEADER, FALSE);
+  // curl_setopt($ch, CURLOPT_HEADER, FALSE);
    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
    $response = curl_exec($ch);
